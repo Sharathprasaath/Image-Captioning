d3e9412fa32f3ffb537c4acf957aea8e140c522b
Sharathprasaath <sharath21110200@snuchennai.edu.in>
2024-09-03 18:54:56 +0530

    Add new-folder with files

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import ImageCaptioning from './page';
+function App() {
+  return (
+    <div className="App">
+      <ImageCaptioning></ImageCaptioning>
+    </div>
+  );
+}
+
+export default App;
